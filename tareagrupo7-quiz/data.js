8fa5354d7c7e64f1c2878b253bde94f59b36f90a
CarlosMartinez <carlosmartinez.xq@gmail.com>
2022-10-08 18:39:14 -0300

    preguntas fueron agregadas al json, en data.json

--- tareagrupo7-quiz/data.js
+++ tareagrupo7-quiz/data.js
@@ -1,20 +1,103 @@
 export const data = [{
-        title: "Before a computer can understand a program it must be...",
+        title: "¿Qué te impulsó a navegar y comprar en nuestra ecommerce?",
         answers: [
-            "Translated into its machine code",
-            "Translated into a low level language",
-            "Translated into a high level language",
+            "Excelente reputación",
+            "Buenos comentarios",
+            "Me la recomendaron",
+            "Por que tiene envió gratis"
         ],
-        correctAnswer: 1,
     },
     {
-        title: "A list of instructions that enable a computer to perform a specific task is a...",
-        answers: ["Computer Program", "Machine Code", "Algorithm", "Binary Code"],
-        correctAnswer: 3,
+        title: "¿Cuáles son los tipos de fallas que tienen nuestros productos que te han provocado una mala experiencia?",
+        answers: ["Defectos de fábrica", "Cajas vienen abiertas", "Productos usados", "Se dañan rápido"],
     },
     {
-        title: "What is the only thing that computers understand?",
-        answers: ["Machine Code", "High Level Languages", "Low Level Languages", "Algorithms"],
-        correctAnswer: 1
-    }
+        title: "¿Qué excusas ha recibido por parte del personal cuando pregunta sobre posibles cambios o devoluciones?",
+        answers: ["No responden", "Que no se hacen cambios", "Que el producto esta usado, ", "Que estaba fuera de plazo"],
+    },
+    {
+        title: "¿Cómo conociste nuestra Tienda?",
+        answers: [
+            "A través de un amigo", "Anuncio en la TV", "En la radio", "Por las RRSS"],
+    },
+    {
+        title: "¿Qué alternativas ofrecemos que te llevó a experimentar con nuestro producto?",
+        answers: [
+            "Accesibilidad", "Buenos descuentos", "Mas variedad", "Mejores marcas"],
+    },
+    {
+        title: "¿Qué palabras usa para hablar de nuestro producto o servicio con sus amigos y familiares?",
+        answers: [
+            "Malas", "No sé", "Muy buenas", "Los odio"],
+    },
+    {
+        title: "¿Consideras que el lenguaje de nuestra marca es el adecuado?",
+        answers: [
+            "NO, no son inclusivos", "Si, por que son inclusivos", "Definitivamente si", "Definitivamente no"],
+    },
+    {
+        title: "¿Cuáles son algunas de las marcas alternativas que nuestros competidores ofrecen y nosotros no?",
+        answers: [
+            "Lacosste", "Nike", "Adidas", "Pumas"],
+    },
+    {
+        title: "¿Qué tan agradable fue tu navegación por nuestra tienda?",
+        answers: [
+            "Horrible", "Muy lenta", "Buena", "espectacular"],
+    },
+    {
+        title: "¿Qué es lo que realmente esperas de nuestro producto al comprarlo?",
+        answers: [
+            "Calidad", "Están de moda", "Que duren", "Resolver una necesidad"],
+    },
+    {
+        title: "¿Qué probabilidades hay de que recomiendes nuestra marca a un amigo y/o familiar?",
+        answers: [
+            "Ninguna", "Probablemente", "Tal vez", "Definitivamente si"],
+    },
+    {
+        title: "¿Por qué recomendarías nuestros productos?",
+        answers: [
+            "Me encantan", "Son Baratos", "Buena Calidad", "Calidad-Precio"],
+    },
+    {
+        title: "¿Por qué no recomiendas nuestros productos?",
+        answers: [
+            "Productos defectuosos", "No eran los de la foto", "Mala calidad", "Precios elevados"],
+    },
+    {
+        title: "¿Nuestros productos te han ayudado a resolver algún problema?",
+        answers: [
+            "Sí", "No"],
+    },
+    {
+        title: "¿Consideras que los medios de comunicación de nuestra marca son los adecuados?",
+        answers: [
+            "Sí", "No", "Se pueden mejorar", "Usen las RRSS"],
+    },
+    {
+        title: "¿Existe relación entre el precio y la calidad del producto/servicio?",
+        answers: [
+            "Sí", "No", "Muy Caro", "Aveces"],
+    },
+    {
+        title: "¿Cómo calificarías tu ultima experiencia con nosotros?",
+        answers: [
+            "Horrible", "Regular", "Buena", "Excelente"],
+    },
+    {
+        title: "¿Cuánto tiempo llevas siendo cliente de nuestra marca?",
+        answers: [
+            "Menos de 3 meses", "Aproximadamente 6 meses", "Menos de 1 año", "Mas de 1 año"],
+    },
+    {
+        title: "¿Nuestra tienda es de fácil acceso?",
+        answers: [
+            "Sí", "No"],
+    },
+    {
+        title: "¿Qué imagen tienes de nuestra marca?",
+        answers: [
+            "Mala", "Regular", "Buena", "Muy Buena"],
+    },
 ];
