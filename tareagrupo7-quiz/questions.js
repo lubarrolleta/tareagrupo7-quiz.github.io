df85d226cc5f88099ba33c190669bef432a5a169
fmoraless <franticzero@gmail.com>
2022-10-12 23:33:50 -0300

    Add questions

--- tareagrupo7-quiz/questions.js
+++ tareagrupo7-quiz/questions.js
@@ -1,4 +1,6 @@
-export let questions = [{
+export let questions = [
+    /* question 0*/
+    {
         title: "¿Cómo evaluarías tu nivel de satisfacción con la empresa?",
         condiciones: [{
                 title: "Muy satisfecho",
@@ -28,7 +30,10 @@ export let questions = [{
 
         ],
         isCondition: false
-    }, {
+    },
+
+    /* question 1*/
+    {
         title: "¿Qué motivo o motivos le han llevado a cancelar el servicio?",
         condiciones: [{
                 title: "La atención al cliente no ha cubierto mis expectativas",
@@ -61,6 +66,7 @@ export let questions = [{
         ],
         isCondition: true
     },
+    /* question 2*/
     {
         title: "¿Cuánto tiempo llevas utilizando los productos/servicios de la empresa?",
         condiciones: [{
@@ -90,6 +96,7 @@ export let questions = [{
         ],
         isCondition: false
     },
+    /* question 3*/
     {
         title: "¿Cuáles son los tipos de fallas que tienen nuestros productos que te han provocado una mala experiencia?",
         condiciones: [{
@@ -114,5 +121,455 @@ export let questions = [{
 
         ],
         isCondition: false
-    }
+    },
+    /* question 4*/
+    {
+        title: "¿Por que recomendarías nuestros productos a tus amigos?",
+        condiciones: [{
+            title: "Me encantan",
+            condition: null //lleva al index de la pregunta
+            },
+
+            {
+                title: "Son economicos",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title: "Buena calidad",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title: "Relacion precio calidad",
+                condition: 5 //lleva al index de la pregunta
+
+            },
+        ],
+        isCondition: false
+    },
+    /* question 5*/
+    {
+        title: "En cuanto a la relación entre el precio y la calidad del producto/servicio. ¿Qué opinión tienes?",
+        condiciones: [{
+            title: "En general, el precio es demasiado alto para la calidad del producto/servicio",
+            condition: null //lleva al index de la pregunta
+        },
+            {
+                title: "En general, el precio es demasiado bajo para la calidad del producto/servicio",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title: "En general, el precio es adecuado para la calidad del producto/servicio",
+                condition: null //lleva al index de la pregunta
+            },
+            {
+                title: "No tengo una opinión clara sobre la relación entre el precio y la calidad del producto/servicio",
+                condition: null //lleva al index de la pregunta
+
+            },
+        ],
+        isCondition: true
+    },
+    /* question 6*/
+    {
+        title: "¿Como calificarías tu ultima experiencia con nosotros?",
+        condiciones: [{
+            title: "Muy buena",
+            condition: null //lleva al index de la pregunta
+        },
+            {
+                title: "Buena",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title: "Regular",
+                condition: null //lleva al index de la pregunta
+            },
+            {
+                title: "Mala",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title: "Muy mala",
+                condition: null //lleva al index de la pregunta
+
+            },
+
+        ],
+        isCondition: false
+    },
+    /* question 7*/
+    {
+        title: "¿Cuánto tiempo llevas siendo cliente de nuestra marca?",
+        condiciones: [{
+            title: "Menos de un mes",
+            condition: null //lleva al index de la pregunta
+        },
+            {
+                title: "Entre 1 y 3 meses",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title: "Entre 3 y 6 meses",
+                condition: null //lleva al index de la pregunta
+            },
+            {
+                title: "Entre 6 meses y un año",
+                condition: null //lleva al index de la pregunta
+            },
+        ],
+        isCondition: false
+    },
+    /* question 8*/
+    {
+        title: "¿Qué imagen tienes de nuestra marca?",
+        condiciones: [{
+            title: "Muy buena",
+            condition: null //lleva al index de la pregunta
+        },
+
+            {
+                title: "Buena",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title: "Regular",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title: "Mala",
+                condition: null //lleva al index de la pregunta
+
+            },
+        ],
+        isCondition: false
+    },
+
+    /* question 9*/
+    {
+        title: "¿Nuestra Tienda es de fácil acceso?",
+        condiciones: [{
+            title: "Si",
+            condition: null //lleva al index de la pregunta
+        },
+
+            {
+                title: "No",
+                condition: 10 //lleva al index de la pregunta
+
+            },
+        ],
+        isCondition: false
+    },
+
+    /* question 10*/
+    {
+        title: "Si respondiste no al facil acceso a nuestra tienda. ¿Por que?",
+        condiciones: [{
+            title: "No hay tienda cerca",
+            condition: null //lleva al index de la pregunta
+        },
+            {
+                title: "No hay tienda en mi ciudad",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title: "No hay tienda en mi país",
+                condition: null //lleva al index de la pregunta
+            },
+            {
+                title: "No hay tienda en mi continente",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title: "No hay tienda en mi planeta",
+                condition: null //lleva al index de la pregunta
+
+            },
+        ],
+        isCondition: true
+    },
+
+
+
+
+
+    /* question 11*/
+    {
+        title: "¿Cómo conociste nuestra tienda?",
+        condiciones: [{
+            title: "Por internet",
+            condition: null //lleva al index de la pregunta
+        },
+
+            {
+                title: "Por recomendación de un amigo",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title: "Por recomendación de un familiar",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title: "Por recomendación de un conocido",
+                condition: 1 //lleva al index de la pregunta
+
+            },
+            {
+                title: "Por recomendación de un vecino",
+                condition: 1 //lleva al index de la pregunta
+
+            },
+
+        ],
+        isCondition: false
+    },
+
+    /* question 12*/
+    {
+        title: "¿Qué tan agradable fue tu navegación por nuestra tienda web?",
+        condiciones: [{
+            title: "Muy agradable",
+            condition: null //lleva al index de la pregunta
+        },
+            {
+                title: "Agradable",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title: "Regular",
+                condition: null //lleva al index de la pregunta
+            },
+            {
+                title: "Mala",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title: "Muy mala",
+                condition: null //lleva al index de la pregunta
+
+            }, {
+                title: "No navegue por la tienda web",
+                condition: null //lleva al index de la pregunta
+
+            },
+
+        ],
+        isCondition: true
+    },
+    /* question 13*/
+    {
+        title: "¿Consideras que el lenguaje de nuestra marca es el adecuado?",
+        condiciones: [{
+            title: "Si",
+            condition: null //lleva al index de la pregunta
+        },
+            {
+                title: "No",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title: "No se",
+                condition: null //lleva al index de la pregunta
+            },
+            {
+                title: "No navegue por la tienda web",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title: "¿Habia una tienda web?",
+                condition: null //lleva al index de la pregunta
+
+            },
+
+        ],
+        isCondition: false
+    },
+    /* question 14*/
+    {
+        title: "¿¿Qué alternativas ofrecemos que te llevó a experimentar con nuestro producto?",
+        condiciones: [{
+            title: "Accesorios",
+            condition: null //lleva al index de la pregunta
+        },
+            {
+                title: "Promociones",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title: "Descuentos",
+                condition: null //lleva al index de la pregunta
+            },
+            {
+                title: "Ninguna",
+                condition: null //lleva al index de la pregunta
+
+            },
+
+
+        ],
+        isCondition: false
+    },
+    /* question 15*/
+    {
+        title: "¿Como evalua la atencio de su ejecutivo?",
+        condiciones: [{
+            title: "Muy buena",
+            condition: null //lleva al index de la pregunta
+        },
+
+            {
+                title: "Buena",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title:"Regular",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title: "Mala",
+                condition: 16 //lleva al index de la pregunta
+
+            },
+        ],
+        isCondition: false
+    },
+    /* question 16*/
+    {
+        title: "¿En que aspectos deberia mejorar la atencio de su ejecutivo?",
+        condiciones: [{
+            title: "No se",
+            condition: null //lleva al index de la pregunta
+        },
+            {
+                title: "Ser atento",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title: "Ser mas amable",
+                condition: null //lleva al index de la pregunta
+            },
+            {
+                title: "Ser mas cordial",
+                condition: null //lleva al index de la pregunta
+
+            },
+        ],
+        isCondition: true
+    },
+    /* question 17*/
+    {
+        title: "¿Cada cuanto tiempo contrata un seguro?",
+        condiciones: [{
+            title: "Cada 6 meses",
+            condition: null //lleva al index de la pregunta
+        },
+            {
+                title: "Cada 1 año",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title: "Cada 2 años",
+                condition: null //lleva al index de la pregunta
+            },
+            {
+                title: "Cada 3 años",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title: "Cada 4 años",
+                condition: null //lleva al index de la pregunta
+
+            },
+
+        ],
+        isCondition: false
+    },
+    /* question 18*/
+    {
+        title: "¿Cuáles son los tipos de fallas que tienen nuestros productos que te han provocado?",
+        condiciones: [{
+            title: "Corte de luz",
+            condition: null //lleva al index de la pregunta
+        },
+            {
+                    title: "Corte de agua",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title: "Corte de gas",
+                condition: null //lleva al index de la pregunta
+            },
+            {
+                title: "Corte de internet",
+                condition: null //lleva al index de la pregunta
+            },
+        ],
+        isCondition: false
+    },
+    /* question 19*/
+    {
+        title: "¿Que caracteristicas buscas en una buena marca?",
+        condiciones: [{
+            title: "Que sea innovadora",
+            condition: null //lleva al index de la pregunta
+        },
+
+            {
+                title: "Que sea confiable",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title: "Que sea de calidad",
+                condition: null //lleva al index de la pregunta
+
+            },
+            {
+                title: "Que sea de precio accesible",
+                condition: null //lleva al index de la pregunta
+
+            },
+        ],
+        isCondition: false
+    },
+
+    /* question 20*/
+    {
+        title: "¿Qué excusas ha recibido por parte del personal cuando pregunta sobre posibles cambios o devoluciones?",
+        condiciones: [{
+            title: "No se",
+            condition: null //lleva al index de la pregunta
+        },
+
+            {
+                title: "No se puede",
+                condition: null //lleva al index de la pregunta
+
+            },
+        ],
+        isCondition: false
+    },
 ]
